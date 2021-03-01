87163b8fc4f025a35ac9cfff283e92b36a225922
Melani Valdez <50336244+Bestobetsa@users.noreply.github.com>
2021-02-28 19:57:10 -0600

    Update Classifier_Bayes.py

--- EjemplosLib/Classifier_Bayes.py
+++ EjemplosLib/Classifier_Bayes.py
@@ -85,7 +85,102 @@ def bagOfWords2VectMN(vocabList,inputSet):
 		if word in vocabList:
 			returnVec[vocabList.index(word)]+=1
 	return returnVec
-
+#Vamos a hacer funcion de Parseo y de testeo
+def textParse(bigString):
+	import re
+	listOfTokens=re.split(r'\W*', bigString)
+	return [tok.lowe() for tok in listOfTokens if len(tok)>2]
+#Classifier_Bayes.spamTest()
+def spamTest():
+	docList=[]
+	classList=[]
+	fullText=[]
+	for i in range(1,26):
+		wordList=textParse(open('email/spam/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList=textParse(open('email/ham/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList=createVocabList(docList)
+	trainingSet=range(50)
+	testSet=[]
+	for i in range(10):
+		randIndex=int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat=[]
+	trainClasses=[]
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vect(vocabList,docList[docIndex]))
+	p0V,p1V,pSpam=trainNB0(array(trainMat),array(trainClasses))
+	errorCount=0
+	for docIndex in testSet:
+		wordVector=setOfWords2Vect(vocabList,docList[docIndex])
+		if classifyNB(array(wordVector),p0V,p1V,pSpam)!=classList[docIndex]:
+			errorCount+=1
+	print("The error rate is:",float((errorCount)/len(testSet)))	
+def calcMostFreq(vocabList,fullText):
+import operator
+freqDict = {}
+for token in vocabList:
+	freqDict[token]=fullText.count(token)
+sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1),\
+reverse=True)
+return sortedFreq[:30]
+def localWords(feed1,feed0):
+	import feedparser
+	docList=[]; classList = []; fullText =[]
+	minLen = min(len(feed1['entries']),len(feed0['entries']))
+	for i in range(minLen):
+		wordList = textParse(feed1['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(feed0['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabList(docList)
+	top30Words = calcMostFreq(vocabList,fullText)
+	for pairW in top30Words:
+		if pairW[0] in vocabList: vocabList.remove(pairW[0])
+	trainingSet = range(2*minLen); testSet=[]
+	for i in range(20):
+		randIndex = int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat=[]; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+	errorCount = 0
+	for docIndex in testSet:
+		wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector),p0V,p1V,pSpam) != \
+		   classList[docIndex]:
+		   errorCount += 1
+print ("the error rate is:",float(errorCount)/len(testSet))
+return vocabList,p0V,p1V
+#Classifier_Bayes.getTopWords(ny,sf)
+def getTopWords(ny,sf):
+	import operator
+	vocabList,p0V,p1V=localWords(ny,sf)
+	topNY=[]; topSF=[]
+	for i in range(len(p0V)):
+		if p0V[i] > -6.0 : topSF.append((vocabList[i],p0V[i]))
+		if p1V[i] > -6.0 : topNY.append((vocabList[i],p1V[i]))
+	sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+	print("SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**") 
+	for item in sortedSF:
+		print item[0]
+	sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+	print("NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY **") 
+	for item in sortedNY:
+		print item[0]
 
 #para probar todo en a linea de comandos de Python3 o Python2
 #from numpy import *
@@ -113,4 +208,7 @@ def bagOfWords2VectMN(vocabList,inputSet):
 #listOfTokens=regEx.split(mySent)
 #emailText = open('email/ham/6.txt').read()
 #listOfTokens=regEx.split(emailText)
-#el archivo 6.txt se encuentra dentro de ham en la capeta email (email.zip)
+#ny=feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+#sf=feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
+#vocabList,pSF,pNY=Classifier_Bayes.localWords(ny,sf)
+#vocabList,pSF,pNY=Classifier_Bayes.localWords(ny,sf)
