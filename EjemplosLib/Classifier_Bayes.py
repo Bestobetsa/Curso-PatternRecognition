eab018d7498ab418c3eb85ec7b58f401f4b35fd3
Melani Valdez <50336244+Bestobetsa@users.noreply.github.com>
2021-02-28 18:32:01 -0600

    Update Classifier_Bayes.py

--- EjemplosLib/Classifier_Bayes.py
+++ EjemplosLib/Classifier_Bayes.py
@@ -6,13 +6,19 @@
 #>>listOPosts,listClasses = bayes.loadDataSet()
 #>>myVocabList = bayes.createVocabList(listOPosts)
 #>>myVocabList
+from numpy import *
+import operator
 def loadDataSet():
-	postingList=[['my','dog','has','flea','problems','help','please'],
-				 ['maybe','not','take','him','to','dog','park','stupid'],
-				 ['my','dalmation','is','so','cute','I','love','him'],
-				 ['stop','posting','stupid','worthless','garbage'],
-				 ['mr','licks','ate','my','steak','how','to','stop','him'],
-				 ['quit','buying','worthless','dog','food','stupid']]
+	postingList=[['my', 'dog', 'has', 'flea', \
+				  'problems', 'help', 'please'],
+				 ['maybe', 'not', 'take', 'him', \
+				  'to', 'dog', 'park', 'stupid'],
+				 ['my', 'dalmation', 'is', 'so', 'cute', \
+				  'I', 'love', 'him'],
+				 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+				 ['mr', 'licks', 'ate', 'my', 'steak', 'how',\
+				  'to', 'stop', 'him'],
+				 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
 	classVec=[0,1,0,1,0,1]	#1 is abusive 0 not
 	return postingList,classVec
 #Ahora haremos la creacion de un datasetvacio y la union de dos
@@ -24,12 +30,41 @@ def createVocabList(dataSet):
 	return list(vocabSet)
 def setOfWords2Vect(vocabList,inputSet):
 	returnVec=[0]*len(vocabList)
-	for word in vocabList:
+	for word in inputSet:
 		if word in vocabList:
 			returnVec[vocabList.index(word)]=1
-		else: print("The Word: %s is not my Vocabulary!"%word)
+		else: 
+			print("The Word: {0} is not my Vocabulary!".format(word))
 	return returnVec
-
-
-
+#ahora vamos a hacer el entranamiento
+def trainNB0(trainMatrix,trainCategory):
+	numTrainDocs=len(trainMatrix)
+	numWords=len(trainMatrix[0])
+	pAbusive=sum(trainCategory)/float(numTrainDocs)
+	p0Num=zeros(numWords)
+	p1NUm=zeros(numWords)
+	p0Denom=0.0
+	p1Denom=0.0
+	for i in range(numTrainDocs):
+		if trainCategory[i]==1:
+			p1NUm+=trainMatrix[i]
+			p1Denom+=sum(trainMatrix[i])
+		else:
+			p0Num+=trainMatrix[i]
+			p0Denom+=sum(trainMatrix[i])
+	p1Vect=p1NUm/p1Denom #change to log()
+	p0Vect=p0Num/p0Denom #change to log()
+	return p0Vect,p1Vect,pAbusive
+#para probar todo en a linea de comandos de Python3 o Python2
+#from numpy import *
+#import Classifier_Bayes
+#listOPosts,listClasses = Classifier_Bayes.loadDataSet()
+#myVocabList = Classifier_Bayes.createVocabList(listOPosts)
+#myVocabList
+#Classifier_Bayes.setOfWords2Vect(myVocabList, listOPosts[0])
+#trainMat=[]
+#for postinDoc in listOPosts: 
+#...
+#	trainMat.append(Classifier_Bayes.setOfWords2Vect(myVocabList, postinDoc))
+#p0V,p1V,pAb=Classifier_Bayes.trainNB0(trainMat,listClasses)
 			 
